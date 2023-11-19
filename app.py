916c20fa38cd85ce0bb916d9896b2f2588b8096b
purvil-patel <purvil1699@gmail.com>
2023-11-18 22:37:56 -0800

    Added Changed files

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import openai
 import os
-import sys
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+import csv
+import PyPDF2
+import pdfplumber
+import pandas as pd
+from werkzeug.utils import secure_filename
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
-
-
 import constant
-
-
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -32,9 +31,19 @@ chain = ConversationalRetrievalChain.from_llm(
     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 
-# In-memory store for chat history (for demonstration purposes)
-# Note: For a production environment, consider using a more persistent storage system
-chat_histories = {}
+# Set the folder for saving uploaded files
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'pdf', 'csv', 'xlsx', 'xls'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Document conversion functions
+# (place the conversion functions from your second script here)
 
 @app.route('/')
 def index():
@@ -43,21 +52,48 @@ def index():
 @app.route('/ask', methods=['POST'])
 def ask():
     user_message = request.form['user_message']
-    #user_id = request.form['user_id']  # Assuming user_id is passed from the client
-
-    # Get or initialize chat history for the user
-    #chat_history = chat_histories.get(user_id, [])
-    chat_history = []
 
     # Process the query using the conversational chain
-    result = chain({"question": user_message, "chat_history": chat_history})
+    result = chain({"question": user_message, "chat_history": []})
     response = result['answer']
 
-    # Update chat history
-    #chat_history.append((user_message, response))
-    #chat_histories[user_id] = chat_history
-
     return jsonify({'response': response})
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+
+        # If user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            # Process the file based on its extension
+            if filename.lower().endswith('.pdf'):
+                text = convert_pdf_to_txt(file_path)
+            elif filename.lower().endswith('.csv'):
+                text = convert_csv_to_txt(file_path)
+            elif filename.lower().endswith(('.xlsx', '.xls')):
+                text = convert_excel_to_txt(file_path)
+
+            # Saving the converted text to a file (optional)
+            output_filename = filename + '.txt'
+            output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+            with open(output_filepath, 'w', encoding='utf-8') as text_file:
+                text_file.write(text)
+
+            return send_from_directory(app.config['UPLOAD_FOLDER'], output_filename, as_attachment=True)
+
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
